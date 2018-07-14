3402c4667e2d0ca8887c3331ee1f29dfdfa2956b
Vetoria <18156255759@163.com>
2018-07-14 21:23:30 +0800

    'recommend'

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img  class="item-img" :src="item.imgUrl">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [ {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1509/ca/2c5c95c035a9f99ad613fad782798aea.water.jpg_150x150_fc63fdb2.jpg',
+        title: '梁子湖龙湾度假村',
+        desc: '梁子湖龙湾度假村旅游圣地'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1710/90/909e5d4a6ac636ada3.img.jpg_150x150_13488beb.jpg',
+        title: '梵高星空艺术馆',
+        desc: '梵高星空艺术馆旅游圣地'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201403/11/1f70100804193c28f8b71378c0faa49e.jpg_150x150_db8e043b.jpg',
+        title: '户部巷',
+        desc: '武汉户部巷美食圣地'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+   .item-img
+     width: 1.7rem
+     height: 1.7rem
+     padding: .1rem
+   .item-info
+     flex: 1
+     padding: .1rem
+     min-width: 0
+     .item-title
+       line-height: .54rem
+       font-size: .32rem
+       ellipsis()
+     .item-desc
+       line-height: .4rem
+       color: #ccc
+       ellipsis()
+     .item-button
+       line-height: .44rem
+       margin-top: .16rem
+       background: #ff9300
+       padding: 0 .2rem
+       border-radius: .06rem
+       color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img  class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [ {
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg',
+        title: '武汉必游TOP10',
+        desc: '到武汉感受神奇的热干面之都'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4f/95a94c90ef3521.jpg_r_640x214_345412f9.jpg',
+        title: '开启亲子时光',
+        desc: '这些地方都是宝贝的最爱'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1507/e0/4aae49007c8a64.jpg_r_640x214_65a42783.jpg',
+        title: '周末出游乐翻天',
+        desc: '春夏秋冬，这里是当地人最爱的休闲时光'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 30.9%
+    .item-img
+       width: 100%
+   .item-info
+     padding: .1rem
+     .item-title
+       line-height: .54rem
+       font-size: .32rem
+       ellipsis()
+     .item-desc
+       line-height: .4rem
+       color: #ccc
+       ellipsis()
+</style>
