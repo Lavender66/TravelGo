94269ff2cd5b701abb9cdbdca6d6629a5d8ef4bc
Vetoria <18156255759@163.com>
2018-07-15 17:18:01 +0800

    'cityAjax'

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,16 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
@@ -25,7 +24,6 @@ export default {
     right: 0
     bottom: 0
     width: .4rem
-    background: red
     .item
       line-hight: .4rem
       text-align: center
