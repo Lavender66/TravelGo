8c9ba3d46d64bcae3006b36f6c114c2ea5575acd
Vetoria <18156255759@163.com>
2018-07-17 19:21:29 +0800

    'xiugai'

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
 import Detail from '@/pages/detail/Detail'
+
 Vue.use(Router)
 
 export default new Router({
